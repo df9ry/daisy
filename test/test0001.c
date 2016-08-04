83f628cde3f8c9e8aa4f1ad5ba59ba162578df79
DF9RY <Tania@DF9RY.de>
2016-08-04 15:20:39 +0200

    Convert to C/C++ Nature Added small test file

---
+++ test/test0001.c
@@ -0,0 +1,17 @@
+/*
+ * test0001.c
+ *
+ *  Created on: 04.08.2016
+ *      Author: tania
+ */
+
+#define _POSIX_SOURCE 1
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello Test0001\n");
+	return(EXIT_SUCCESS);
+}
